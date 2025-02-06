ea04e473a8139bef05b99df59add0e804fa9b9b2
Mkajitha <146511124+Mkajitha@users.noreply.github.com>
2025-02-06 13:38:32 +0530

    Update main.dart

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 void main() {
   runApp(const MainApp());
@@ -17,7 +18,7 @@ class MainAppState extends State<MainApp> {
   String operator = "";
 
   Widget calc(String text, Color bgcolor) {
-    double size = MediaQuery.of(context).size.width / 5;
+    double size = MediaQuery.of(context).size.width / 7;
 
     return InkWell(
       child: Container(
@@ -29,11 +30,11 @@ class MainAppState extends State<MainApp> {
         margin: const EdgeInsets.all(10),
         height: size,
         width: size,
-        child: Text(
+        child: AutoSizeText(
           text,
           style: const TextStyle(
             color: Colors.white,
-            fontSize: 30,
+            fontSize: 100,
           ),
         ),
       ),
@@ -78,7 +79,8 @@ class MainAppState extends State<MainApp> {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
-        body: SingleChildScrollView(  // Wrapping the entire body in a scrollable widget
+        body: SingleChildScrollView(
+          // Wrapping the entire body in a scrollable widget
           child: Column(
             children: [
               // Input value with horizontal scroll to prevent overflow
@@ -86,7 +88,8 @@ class MainAppState extends State<MainApp> {
                 alignment: Alignment.bottomRight,
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal, // Horizontal scrolling for long input
+                  scrollDirection:
+                      Axis.horizontal, // Horizontal scrolling for long input
                   child: Text(
                     inputValue,
                     style: const TextStyle(
@@ -100,46 +103,50 @@ class MainAppState extends State<MainApp> {
               Container(
                 // Adding padding to prevent the buttons from going off-screen
                 padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        calc("7", Colors.white54),
-                        calc("8", Colors.white54),
-                        calc("9", Colors.white54),
-                        calc("/", Colors.orange),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        calc("4", Colors.white54),
-                        calc("5", Colors.white54),
-                        calc("6", Colors.white54),
-                        calc("*", Colors.orange),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        calc("1", Colors.white54),
-                        calc("2", Colors.white54),
-                        calc("3", Colors.white54),
-                        calc("-", Colors.orange),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        calc("0", Colors.white54),
-                        calc(".", Colors.white54),
-                        calc("=", Colors.orange),
-                        calc("+", Colors.orange),
-                      ],
-                    ),
-                    calc("clear", Colors.black),
-                  ],
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          calc("7", Colors.white54),
+                          calc("8", Colors.white54),
+                          calc("9", Colors.white54),
+                          calc("/", Colors.orange),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          calc("4", Colors.white54),
+                          calc("5", Colors.white54),
+                          calc("6", Colors.white54),
+                          calc("*", Colors.orange),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          calc("1", Colors.white54),
+                          calc("2", Colors.white54),
+                          calc("3", Colors.white54),
+                          calc("-", Colors.orange),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          calc("0", Colors.white54),
+                          calc(".", Colors.white54),
+                          calc("=", Colors.orange),
+                          calc("+", Colors.orange),
+                        ],
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                        calc("clear", Colors.black),
+                      ]),
+                    ],
+                  ),
                 ),
               ),
             ],
